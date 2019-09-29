cce752d3bb899b0b1acab97bd6131870e7e777d5
Akira Matsuda <ronnie@dio.jp>
2019-09-29 11:12:48 +0900

    Initial implementation of I18nCache

---
+++ lib/speed_king/i18n_cache.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module I18nCache
+  def self.extended(obj)
+    obj.instance_variable_set :@i18n_cache, {}
+  end
+
+  def translate(*args, **options)
+    cache = @i18n_cache[I18n.locale] ||= {}
+    cache_key = args << options
+
+    cache[cache_key] || begin
+      result = super
+      cache[cache_key] = result if result
+      result
+    end
+  end
+  alias t translate
+end
+
+I18n.extend I18nCache
