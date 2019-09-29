dbd9c25238e69dc6e210e6d4d694b4f1d27e622c
Akira Matsuda <ronnie@dio.jp>
2019-09-29 11:15:35 +0900

    Tie them up!

--- lib/speed_king.rb
+++ lib/speed_king.rb
@@ -1,6 +1,6 @@
-require "speed_king/version"
+# frozen_string_literal: true
 
-module SpeedKing
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require "speed_king/version"
+require_relative 'speed_king/i18n_cache'
+require_relative 'speed_king/inflector_cache'
+require_relative 'speed_king/layout_cache'
